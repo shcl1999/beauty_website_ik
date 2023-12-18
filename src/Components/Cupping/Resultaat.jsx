effab1c8310c3d69cd020a7928d7986d35a5e59e
denny-038 <141055309+denny-038@users.noreply.github.com>
2023-12-18 14:33:38 +0100

    change cupping result

--- src/Components/Cupping/Resultaat.jsx
+++ src/Components/Cupping/Resultaat.jsx
@@ -3,9 +3,9 @@ import Before from '../../Assets/Cupping/Cuppingbefore.png'
 import After from '../../Assets/Cupping/CuppingAfter.png'
 const Resultaat = () => {
   return (
-    <div className="bg-[#f7f1ea] flex flex-col items-start min-h-screen">
-      <h1 className='ml-[100px] text-4xl mt-9'>Resultaat met Bellabaci cupping</h1>
-      <div className='md:w-full flex flex-col md:flex-row justify-around items-center md:ml-5 ml-[115px] mt-[120px] md:space-y-0 space-y-12'>
+    <div className="bg-[#f7f1ea] flex flex-col items-start min-h-screen" style={{minHeight: '85vh'}}>
+      <h1 className='md:ml-[100px] ml-[80px] md:text-4xl text-3xl mt-9'>Resultaat met Bellabaci cupping</h1>
+      <div className='md:w-full flex flex-col md:flex-row justify-around items-center md:ml-5 ml-[115px] md:mt-[100px] mt-[60px] md:space-y-0 space-y-12'>
         <img src={Before} alt="Bellabaci Product 1" className="object-contain md:h-[350px] md:w-auto h-48 w-auto mr-6"/>
         <img src={After} alt="Bellabaci Product 2" className="object-contain md:h-[350px] md:w-auto h-48 w-auto mr-6"/>
       </div>
