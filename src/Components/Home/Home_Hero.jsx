86b27109335f1d7ef1b877f3dce299d347ebfab6
denny-038 <141055309+denny-038@users.noreply.github.com>
2023-12-21 15:47:15 +0100

    new version

--- src/Components/Home/Home_Hero.jsx
+++ src/Components/Home/Home_Hero.jsx
@@ -14,15 +14,15 @@ const Hero = () => {
   }
 
   return (
-    <div className="bg-[#D7C7B5] w-full bg-center bg-no-repeat h-screen items-center relative flex flex-col">
+    <div className="bg-[#D7C7B5] min-w-screen max-w-screen bg-center bg-no-repeat h-screen items-center relative flex flex-col">
       <img src={Logo} alt="Logo" 
-           className="mt-6 md:absolute md:left-2 md:mt-6 md:ml-4 md:md:ml-10" 
+           className="mt-6 md:absolute md:left-2 md:mt-6 md:ml-10" 
            style={{ width: '70px', height: 'auto' }} />
       
       <div className='lg:flex top-1 left-0 right-0 justify-center items-center h-[114px] text-white'>
-        <ul className='text-xl flex md:space-x-[60px] text-md text-white pb-4'>
-          <li onClick={() => handleNavigate('/')}className='p-4 cursor-pointer'>Home</li>
-          <li className="dropdown p-4 cursor-pointer"><HashLink smooth to='#Behandelingen'>Behandelingen</HashLink>
+        <ul className='text-xl flex md:space-x-[60px] text-md text-white pb-4 md:mt-3.5'>
+        <li onClick={() => handleNavigate('/')}className='p-3 cursor-pointer'>Home</li>
+        <li className="dropdown p-3 cursor-pointer">Behandelingen
             <div className="dropdown-content">
               <HashLink className='text-lg' smooth to='/Pedicure'>Pedicure</HashLink>
               <HashLink className='text-lg' smooth to='/Schoonheid'>Schoonheid</HashLink>
@@ -30,8 +30,8 @@ const Hero = () => {
               <HashLink className='text-lg' smooth to='/Voetreflex'>Voetreflex</HashLink>
             </div>
           </li>
-          <li onClick={() => handleNavigate('/Tarieven')} className='p-4 cursor-pointer'>Tarieven</li>
-          <li className='p-4'><HashLink smooth to='#Contact'>Contact</HashLink></li>
+          <li onClick={() => handleNavigate('/Tarieven')} className='p-3 cursor-pointer'>Tarieven</li>
+          <li className='p-3'><HashLink smooth to='/#Contact'>Contact</HashLink></li>
         </ul>
       </div>
         
