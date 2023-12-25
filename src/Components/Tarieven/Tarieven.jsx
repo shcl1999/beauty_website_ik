91ca0c26e496d953d263639685572d7ef056b664
shcl1999 <sunnynate@hotmail.com>
2023-12-25 19:40:24 +0100

    adjusted some minor typos in tarieven

--- src/Components/Tarieven/Tarieven.jsx
+++ src/Components/Tarieven/Tarieven.jsx
@@ -5,7 +5,7 @@ const Tarieven = () => {
     <div className="bg-[#f7f1ea] flex flex-col min-h-screen " style={{minHeight:'100%'}}>
     <div className='md:ml-[200px] ml-5 mt-10 md:mr-[190px] mr-2'>
     <h1 className='md:text-[66px] text-4xl font-bold text-[#462D10] md:mb-10'>Massage</h1>
-      <p className='md:text-[28px] text-lg font-semibold mb-8'>Huid verbeterende massages</p>
+      <p className='md:text-[28px] text-lg font-semibold mb-8'>Huidverbeterende massages</p>
       <p className='md:text-[22px] text-md font-semibold'>Bindweefsel gezicht & decolleté <span className='md:text-[16px] text-sm font-light'>(45 minuten)</span><span className='font-bold md:ml-[491px] ml-[25px]'>€60,-</span></p>
       <p className='italic md:text-[16px] text-sm font-light mb-5'>Oppervlakte reiniging / bindweefselmassage / dag verzorging</p>
       <p className='md:text-[22px] text-md font-semibold '>Guasha gezichtsmassage <span className='md:text-[16px] text-sm font-light'>(45 minuten)</span><span className='font-bold md:ml-[568px] ml-[80px]'>€60,-</span></p>
@@ -31,12 +31,11 @@ const Tarieven = () => {
     <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping cellulite</p>
     <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping anti-aging </p>
     <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping stress </p>
-    <p className='md:text-[22px] text-md font-semibold mb-[40px]'>Bellabaci cupping buik </p>
 
-    <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping kuur <span className='md:text-[16px] text-sm font-light'>30 min 7+1 gratis</span><span className='font-bold md:ml-[548px] ml-[70px]'>€440,-</span></p>
+    <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping kuur <span className='md:text-[16px] text-sm font-light'>30 min 7+1 gratis</span><span className='font-bold md:ml-[548px] ml-[70px]'>€520,-</span></p>
     <p className='md:text-[22px] text-md font-semibold'>Bellabaci cupping kuur <span className='md:text-[16px] text-sm font-light'>60 min 7+1 gratis</span><span className='font-bold md:ml-[548px] ml-[69px]'>€604,-</span></p>
     <p className='md:text-[22px] text-md font-semibold mb-9'>Bellabaci cupping kuur <span className='md:text-[16px] text-sm font-light'>75 min 7+1 gratis</span><span className='font-bold md:ml-[551px] ml-[71px]'>€708,-</span></p>
-    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-9'>De kuren zijn inclusief een Bellabaci tasje (genie + 2 cups, harde en een zachte) en 8 weken lang online begeleiding inclusief 21 video’s E-book en een voortgangskaart t.w.v. €100.</p>
+    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-9'>De kuren zijn d.m.v. een Bellabaci tasje (genie + 2 cups, harde en een zachte) en 8 weken lang online begeleiding inclusief 21 video’s E-book en een voortgangskaart t.w.v. €100.</p>
     <p className='md:text-[28px] text-[20px] font-semibold mb-8'>Speciale voetreflexmassages</p>
     <p className='md:text-[22px] text-md font-semibold'>Metamorfose voetreflexmassage  
     <span className='font-bold md:ml-[592px] ml-[233px]'>€80,-</span></p>
@@ -50,16 +49,16 @@ const Tarieven = () => {
     <div className='md:w-[105%] w-[95%] h-[1px] bg-[#023A15] mx-auto my-12'></div>
     <h1 className='md:text-[66px] text-4xl font-bold text-[#462D10] md:mb-10'>Pedicure</h1>
     <p className='md:text-[32px] text-lg font-semibold mb-8'>Pedicurebehandelingen</p>
-    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling <span className='md:text-[16px] text-sm font-light'>(30-45 minuten)</span><span className='font-bold md:ml-[586px] ml-[95px]'>€45,-</span></p>
+    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling Basis<span className='md:text-[16px] text-sm font-light'>(30-45 minuten)</span><span className='font-bold md:ml-[586px] ml-[95px]'>€45,-</span></p>
     <p className='italic md:text-[16px] text-sm font-light'>Nagels Knippen en verzorgen nagelomgeving/ verwijderen overtollig eelt / ingegroeide </p>
     <p className='italic md:text-[16px] text-sm font-light mb-5'>nagel(s) en- of schimmelnagels/ verwijderen likdoorns/ verzorgende voeten crème </p>
     <p className='md:text-[22px] text-md font-semibold mb-5'>Pedicurebehandeling Complex  <span className='md:text-[16px] text-sm font-light'>(45-60 minuten)</span><span className='font-bold md:ml-[478px] ml-[16px]'>€58,-</span></p>
     <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling Luxe <span className='md:text-[16px] text-sm font-light'>(75 minuten)</span><span className='font-bold md:ml-[552px] ml-[74px]'>€80,-</span></p>
     <p className='italic md:text-[16px] text-sm font-light mb-5'>Pedicurebehandeling, scrub en voedende , hydraterend masker van Strictly Pure en voetmassage </p>
 
-    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling diabetische/reumatische voet</p>
+    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling diabetische/reumatische voet <span className='font-bold md:ml-[508px] ml-[57px]'>€50,-</span></p> 
     <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling reumatische voet <span className='font-bold md:ml-[508px] ml-[57px]'>€50,-</span></p>
-    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling overige risico voeten</p>
+    <p className='md:text-[22px] text-md font-semibold'>Pedicurebehandeling overige risico voeten <span className='font-bold md:ml-[508px] ml-[57px]'>€50,-</span></p>
     <p className='md:text-[22px] text-md font-semibold'>Deelbehandeling <span className='md:text-[16px] text-sm font-light'>(15 minuten)</span><span className='font-bold md:ml-[669px] ml-[159px]'>€25,-</span></p>
     <p className='italic md:text-[16px] text-sm font-light mb-5'>In de deelbehandeling wordt 1 probleem aangepakt, bijv. 1 likdoorn of 1 kloof</p>
 
@@ -75,11 +74,11 @@ const Tarieven = () => {
     <p className='italic md:text-[16px] text-sm font-light mb-[100px] mr-3'>*Nagellak verkrijgbaar in de praktijk. Nagels kunnen alleen gelakt worden als u slippers heeft.</p>
     <p className='text:text-[28px] text-[20px] font-semibold mb-8 text-[#462D10]'>Manicurebehandelingen*</p>
     <p className='md:text-[22px] text-md font-semibold'>Parafine manicure <span className='md:text-[16px] text-sm font-light'>(45 minuten)</span><span className='font-bold md:ml-[649px] ml-[145px]'>€45,-</span></p>
-    <p className='md:mr-[200px] italic md:text-[18px] text-sm font-light'>Desinfecteren van de handen, nagelriemen verzorgen, nagels knippen, vijlen, scrub, ontspannende handmassage, handcrème, parafine bad.</p>
+    <p className='md:mr-[200px] italic md:text-[18px] text-sm font-light'>Nagelriemen verzorgen, nagels knippen, vijlen, scrub, handmassage, handcrème, parafine bad.</p>
     <p className='md:text-[22px] text-md font-semibold'>Nagels handen vijlen/knippen <span className='font-bold md:ml-[620px] ml-[139px]'>€15,-</span></p>
     <p className='md:text-[22px] text-md font-semibold'>Ontspannende handmassage <span className='md:text-[16px] text-sm font-light'>(25 minuten)</span><span className='font-bold md:ml-[524px] ml-[52px]'>€30,-</span></p>
     <p className='italic md:text-[16px] text-sm font-light mb-4'>Inclusief handcrème</p>
-    <p className='italic md:text-[16px] text-sm font-light mb-[100px]'>*De manicurebehandelingen zijn alleen mogelijk in combinatie met een andere behandeling</p>
+    <p className='italic md:text-[16px] text-sm font-light mb-[100px]'>*Manicurebehandelingen zijn alleen mogelijk in combinatie met een andere behandeling</p>
     <div className='md:w-[105%] w-[95%] h-[1px] bg-[#023A15] mx-auto my-12'></div>
 
     <h1 className='md:text-[66px] text-4xl font-bold text-[#462D10] mb-12'>Voetreflextherapie</h1>
@@ -87,15 +86,15 @@ const Tarieven = () => {
     <div className='md:w-[105%] w-[95%] h-[1px] bg-[#023A15] mx-auto my-12'></div>
 
     <h1 className='md:text-[66px] text-4xl font-bold text-[#462D10] mb-12'>Schoonheid</h1>
-    <p className='md:text-[22px] text-md font-semibold'>Huidverbeterende schoonheidsbehandeling basis <span className='md:text-[16px] text-sm font-light'>(60 minuten)</span><span className='font-bold md:ml-[288px] ml-[330px]'>€75,-</span></p>
-    <p className='md:mr-[210px] italic md:text-[16px] text-sm font-light mb-5'>Intensieve reiniging met peeling of scrub, stomen, verwijderen van onzuiverheden,epileren van de wenkbrauwen, masker, tijdens het masker massage van decolleté, nek en schouders of een uitgebreide handmassage, dag/nacht verzorging</p>
+    {/* <p className='md:text-[22px] text-md font-semibold'>Huidverbeterende schoonheidsbehandeling basis <span className='md:text-[16px] text-sm font-light'>(60 minuten)</span><span className='font-bold md:ml-[288px] ml-[330px]'>€75,-</span></p>
+    <p className='md:mr-[210px] italic md:text-[16px] text-sm font-light mb-5'>Intensieve reiniging met peeling of scrub, stomen, verwijderen van onzuiverheden,epileren van de wenkbrauwen, masker, tijdens het masker massage van decolleté, nek en schouders of een uitgebreide handmassage, dag/nacht verzorging</p> */}
     <p className='md:text-[22px] text-md font-semibold'>Huidverbeterende schoonheidsbehandeling plus <span className='md:text-[16px] text-sm font-light'>(90 minuten)</span><span className='font-bold md:ml-[285px] ml-[320px]'>€100,-</span></p>
-    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-5'>Analyse van de huid, oppervlakte reinigen, diepte reiniging, onzuiverheden verwijderen, eventueel wenkbrauwen bijwerken, klassieke gezichtsmassage*, masker, serum, oogcrème, dag/nacht verzorging</p>
+    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-5'>Analyse van de huid, oppervlakte reinigen, dieptereiniging, onzuiverheden verwijderen, eventueel wenkbrauwen bijwerken, klassieke gezichtsmassage*, masker, serum, oogcrème, dag/nacht verzorging</p>
 
     <p className='md:text-[22px] text-md font-semibold'>Huidverbeterende schoonheidsbehandeling luxe <span className='md:text-[16px] text-sm font-light'>(90 minuten)</span><span className='font-bold md:ml-[293px] ml-[325px]'>€115,-</span></p>
     <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-5'>Analyse van de huid, oppervlakte reinigen, diepte reiniging, onzuiverheden verwijderen, eventueel wenkbrauwen bijwerken, klassieke gezichtsmassage*, ampul, eye patches, peel-off alginaat masker, serum, oogcrème, dag/nacht verzorging</p>
 
-    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-[100px]'>*Bij de twee schoonheidsbehandelingen is het ook mogelijk om ipv de klassieke gezichtsmassage een guasha massage of de Japanse gezichtsmassage uit te laten voeren tegen een meerprijs van €12.50.</p>
+    <p className='md:mr-[200px] italic md:text-[16px] text-sm font-light mb-[100px]'>*Bij schoonheidsbehandelingen is het mogelijk om ipv de klassieke gezichtsmassage een guasha massage of de Japanse gezichtsmassage uit te laten voeren tegen een meerprijs van €12.50.</p>
     <p className='md:text-[22px] text-md font-semibold mb-[60px]'>Wenkbrauwen epileren <span className='font-bold md:ml-[680px] ml-[180px]'>€12,50</span></p>
     <p className='md:text-[22px] text-md font-semibold'>Bovenlip of kin waxen <span className='font-bold md:ml-[698px] ml-[192px]'>€10,-</span></p>
     <p className='md:text-[22px] text-md font-semibold mb-[60px]'>Bovenlip en kin waxen  <span className='font-bold md:ml-[692px] ml-[188px]'>€18,-</span></p>
